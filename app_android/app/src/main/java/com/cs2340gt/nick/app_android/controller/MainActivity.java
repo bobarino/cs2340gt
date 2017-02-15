557e5f0b74b33fb086d09fa623dbb837bc75485a
Armando Gonzalez <armando.gonzalez97@gmail.com>
2017-02-15 10:08:35 -0500

    Added controller directory and relevant files

---
+++ app_android/app/src/main/java/com/cs2340gt/nick/app_android/controller/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cs2340gt.nick.app_android.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cs2340gt.nick.app_android.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
