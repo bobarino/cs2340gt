8ced815e4c25f0825095c6325fd9181215211441
Elizabeth McConnell <elizabethcmcconnell@gmail.com>
2017-02-15 19:21:11 -0500

    welcome screen

--- app_android/app/src/main/java/com/cs2340gt/nick/app_android/WelcomeActivity.java
+++ app_android/app/src/main/java/com/cs2340gt/nick/app_android/WelcomeActivity.java
@@ -3,12 +3,13 @@ package com.cs2340gt.nick.app_android;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-
-public class MainActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_welcome);
+
+
     }
 }
