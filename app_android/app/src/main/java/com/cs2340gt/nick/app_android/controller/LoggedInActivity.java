835905bfcacccecc4eabea749178a21920d3d4e8
Sean Bills <SEAN@lawn-143-215-80-235.lawn.gatech.edu>
2017-02-15 21:26:28 -0500

    created a physical logged in page and the associated xml file

--- app_android/app/src/main/java/com/cs2340gt/nick/app_android/controller/LoggedInActivity.java
+++ app_android/app/src/main/java/com/cs2340gt/nick/app_android/controller/LoggedInActivity.java
@@ -1,13 +1,14 @@
-package com.cs2340gt.nick.app_android;
+package com.cs2340gt.nick.app_android.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.cs2340gt.nick.app_android.R;
 
-public class MainActivity extends AppCompatActivity {
+public class LoggedInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.logged_in);
     }
 }
