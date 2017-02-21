8e89dd3a314880ddcb91f490db19885f40b251f4
Sean Bills <SEAN@lawn-143-215-80-235.lawn.gatech.edu>
2017-02-20 19:19:08 -0500

    updated it so that registering a new user actually allows for proper login with that user (minor fix to login code)

--- app_android/app/src/main/java/com/cs2340gt/nick/app_android/controller/MainActivity.java
+++ app_android/app/src/main/java/com/cs2340gt/nick/app_android/controller/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent =
-                        new Intent(getBaseContext(), EditAccountInfoActivity.class);
+                        new Intent(getBaseContext(), RegistrationActivity.class);
                 startActivity(intent);
             }
         });
